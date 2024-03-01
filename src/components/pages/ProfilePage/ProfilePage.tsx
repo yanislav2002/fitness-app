e8a704b6c79a8923029ad8571e0d4e248b4433f4
Yanislav <114386411+yanislav2002@users.noreply.github.com>
2024-03-01 13:01:31 +0200

    add additional components into profile page

--- src/components/pages/ProfilePage/ProfilePage.tsx
+++ src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import './ProfilePage.css';
 import ProfileInfo from '../../ProfileInfo/ProfileInfo';
 import PlanItem from '../../PlanItem/PlanItem';
+import StatisticItem from '../../StatisticItem/StatisticItem';
 
 
 export default function ProfilePage():JSX.Element {
@@ -9,19 +10,31 @@ export default function ProfilePage():JSX.Element {
             <ProfileInfo />
 
             <section className='user-plans'>
+                
+                <div className='plan-container'>
 
-                <div className="plans">
-                    <PlanItem />
-                    <PlanItem />
-                    <PlanItem />
-                    <PlanItem />
-                    <PlanItem />
-                    <PlanItem />
-                    <PlanItem />
+                    <h2>My plans</h2>
+                    <div className="plan-list">
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                        <PlanItem />
+                    </div>
                 </div>
 
-                <div className="plans">
-                    
+                <div className='plan-container'>
+
+                    <h2>History</h2>
+                    <div className="plan-list">
+                        <StatisticItem />
+                        <StatisticItem />
+                        <StatisticItem />
+                    </div>
                 </div>
 
             </section>
